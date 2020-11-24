fc2c0ae917cec6fbfa7e05cb3e913d6fa873692b
Louis <louis14@live.be>
2020-11-24 17:32:56 +0100

    Test MAx fonctione stp
    allez

--- main.cpp
+++ main.cpp
@@ -2,9 +2,10 @@
 
 
 
-//Test 2
+//TEST 3
 int main()
 {
+
     sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
